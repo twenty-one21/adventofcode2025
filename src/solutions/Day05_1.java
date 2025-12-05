22e5cbc250376a4c54b0348e2ed4c4cfc8bbe3f6
twenty-one21 <31107399+twenty-one21@users.noreply.github.com>
2025-12-04 20:47:25 -0800

    refactor

--- src/solutions/Day05_1.java
+++ src/solutions/Day05_1.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-class Day00 {
+class Day05_1 {
     public static void main(String[] args) throws IOException {
-//        String input = Files.readString(Path.of("src/inputs/___"));
+        String input = Files.readString(Path.of("src/inputs/day05_test.txt"));
 //        Stream<String> stream = input.lines();
         System.out.println("Hello world!");
     }
